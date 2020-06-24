80c4183b94d8a241a409f33c1acd0e64294cb118
compotypo <andrew.ja.jensen48@gmail.com>
2020-06-24 00:34:16 -0500

    csvConverter expanded

--- csvToSql.py
+++ csvToSql.py
@@ -2,52 +2,80 @@ import sys
 import re
 import os
 
+
+def writeTableData(data, out):
+    for r in data.split('\n'):
+        out.write(r + '\n')
+        r = data.split('\n')
+
+
 def buildTableData(csv, f, attrs, out):
     r = csv.readline()
+    data = ''
+    types = [None] * len(attrs)
     while True:
-        values = []
-        types = []
         if r == '':
             break
+        values = []
+        i = 0
         for col in r.split(','):
             val = col.replace(' ', '_').strip('\"\n')
-            if re.match('^\d+$', val):
-                types.append('INTEGER')
-            elif re.match('[+-]?([0-9]*[.])?[0-9]+'):
-                types.append('FLOAT')
+            if re.match(r'^\d+$', val):
+                types[i] = 'INTEGER'
+            elif re.match(r'[+-]?([0-9]*[.])?[0-9]+', val):
+                types[i] = 'FLOAT'
             values.append(val)
-        
-        out.write('INSERT INTO ' + f + ' (' + str(attrs).strip('\[\'\'\]').replace("'", "") + ') VALUES (' + str(values).strip('\'\[\]\'').replace("'", "") + ');\n')
+            i += 1
+        data += 'INSERT INTO ' + f + ' (' + str(attrs).replace("'", "").strip(
+            r'\[\]\n') + ') VALUES (' + str(values).strip(r'\[\]').replace("'", "") + ');\n'
         r = csv.readline()
 
-def buildTable(dir, f, out):
-    out.write('DROP TABLE IF EXISTS ' + f[:-4] + ';\n')
-    out.write('CREATE TABLE ' + f[:-4] + ' (\n')
-    csv = open(dir + f, 'r')
-    headers = csv.readline().split(',')
-    for i in range(len(headers)):
-        headers[i] = headers[i].replace(' ', '_').strip('\"\n')
-        line_comma = ',' if len(headers)-1 != i else ''
-        if headers[i] == "":
-            headers[i] = 'id'
-        if headers[i].find('id') >= 0 or headers[i].find('num') >= 0:
+    return data, types
+
+
+def buildTable(f, out):
+    f_name = re.search(r'[ \w-]+?(?=\.)', f).group(0)
+    out.write('DROP TABLE IF EXISTS ' + f_name + ';\n')
+    out.write('CREATE TABLE ' + f_name + ' (\n')
+    csv = open(f, 'r')
+    raw_headers = csv.readline().split(',')
+    data, pred_types = buildTableData(csv, f_name, raw_headers, out)
+    for i in range(len(raw_headers)):
+        raw_headers[i] = raw_headers[i].replace(' ', '_').strip('\"\n')
+        line_comma = ',' if len(raw_headers)-1 != i else ''
+        if raw_headers[i] == "":
+            raw_headers[i] = 'index'
+        if raw_headers[i].find('id') >= 0 or raw_headers[i].find('num') >= 0:
             t = ' BIGINT UNSIGNED'
-            t += ',' if headers[i] != 'id' else ' NOT NULL AUTO_INCREMENT PRIMARY KEY' + line_comma
-        elif headers[i].find('date') >= 0: 
+            t += ',' if raw_headers[i] != 'id' else ' NOT NULL AUTO_INCREMENT PRIMARY KEY' + line_comma
+        elif pred_types[i] != None:
+            t = ' ' + pred_types[i] + line_comma
+        elif raw_headers[i].find('date') >= 0:
             t = ' TIMESTAMP NOT NULL' + line_comma
         else:
             t = ' VARCHAR(255)' + line_comma
-        out.write('\t' + headers[i].replace(' ', '_') + t + '\n')
+        out.write('\t' + raw_headers[i].replace(' ', '_') + t + '\n')
     out.write(');\n\n')
-    buildTableData(csv, f[:-4], headers, out)
+    writeTableData(data, out)
 
-def getFileName():
-    return input('Enter a better filename for the schema file: ')
+
+def getFileName(unreadable):
+    return input('Enter a better filename for ' + unreadable + ' the schema file: ')
+
+
+def readDir(path, out):
+    for dirpath, subdirs, files in os.walk(path):
+        for dir in subdirs:
+            readDir(dirpath + dir, out)
+        for f in files:
+            if f[-4:] == '.csv':
+                buildTable(path + f, out)
+
 
 def createSchema(dir):
     out = ''
     if dir == './':
-        file_name = getFileName()
+        file_name = getFileName(dir)
         out = open('./' + file_name + '.sql', 'w+')
         out.write('DROP SCHEMA IF EXISTS ' + file_name + ';\n')
         out.write('CREATE SCHEMA ' + file_name + ';\n\n')
@@ -57,14 +85,16 @@ def createSchema(dir):
         out.write('DROP SCHEMA IF EXISTS ' + dir + ';\n')
         out.write('CREATE SCHEMA ' + dir + ';\n\n')
         out.write('USE ' + dir + ';\n\n')
+    return out
 
-    for subdir, dirs, files in os.walk(dir):
-        for f in files:
-            if f[-4:] == '.csv':
-                buildTable(dir, f, out)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('ERR format: python3 csvToSql.py <path/to/csv>')
-    else:
-        createSchema(sys.argv[1])
+        exit(1)
+
+    if os.path.isfile(sys.argv[1]):
+        buildTable(sys.argv[1], getFileName(sys.argv[1]))
+    elif os.path.isdir(sys.argv[1]):
+        outfile = createSchema(sys.argv[1])
+        readDir(sys.argv[1], outfile)
